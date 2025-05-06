f294ec1842b19fd3e0038b964cde16045ca37bbb
snehashah78 <11snehashah@gmail.com>
2025-05-06 23:16:36 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,16 @@
+package Pattterns;
+
+public class Pattern2 {
+    
+    public static void main(String[] args) {
+        int n=4;
+       for(int i=1;i<=n;i++)     // no. of loop 4//outer loop
+       {
+for(int j=65;j<=70;j++)
+{
+System.out.print((char)(j));         // type casting
+} 
+System.out.println();      // no. of column or printing
+       }
+    }
+}
