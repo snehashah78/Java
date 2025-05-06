3542ffacf1000d2bdede9f299964dd1bbde102a8
snehashah78 <11snehashah@gmail.com>
2025-05-06 23:22:45 +0530

    Add files via upload

---
+++ Pattern41.java
@@ -0,0 +1,16 @@
+package Pattterns;
+
+public class Pattern41 {
+    public static void main(String[] args) {
+        {
+            for(int i=0;i<=5;i++)
+            {
+                for(int j=0;j<=i+3;j++)
+                { 
+                    System.out.print("*");
+                }
+                System.out.println();
+            } 
+    }
+}
+}
