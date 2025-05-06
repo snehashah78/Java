7a144e45e11ccfb9e368ab33ab2ec6d9a3c2f49a
snehashah78 <11snehashah@gmail.com>
2025-05-06 23:18:55 +0530

    Add files via upload

---
+++ P2copy.java
@@ -0,0 +1,15 @@
+package Pattterns;
+
+public class P2copy {
+    public static void main(String[] args) {
+        int n=4;
+       for(int i=1;i<=n;i++)     // no. of loop 4//outer loop
+       {
+for(char j='A';j<='E';j++)
+{
+System.out.print(j);
+} 
+System.out.println();      // no. of column or printing
+       }
+    }
+}
