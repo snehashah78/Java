65fdf46aff8dec71d28c20542b7f0830055380ca
snehashah78 <11snehashah@gmail.com>
2025-05-06 23:19:39 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,17 @@
+package Pattterns;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n=3; int a=1;
+        {
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=n;j++)
+                {
+                    System.out.print(a);
+                    a++;
+                } System.out.println();
+            }
+        }  
+    }
+}
